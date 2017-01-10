b009f18d0a7a9a28296cf96420e0b27151e43fe5
claudiopro <claudio.procida@gmail.com>
2017-01-10 12:19:14 +0100

    Watches erb templates for hologram build

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,17 @@ const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const hologram = require('./gulp-hologram');
 
-const SASS_SOURCES = './sass/*.scss';
-const STYLE_GUIDE_SOURCES = ['./sass/index.md', './doc_assets/*.html'].concat(SASS_SOURCES);
 const DEST = './css';
 const DIST = './dist';
-
-gulp.task('default', ['sass', 'hologram', 'watch']);
+const SASS_SOURCES = './sass/*.scss';
+const STYLE_GUIDE_SOURCES = [
+  './sass/index.md',
+  './doc_assets/*.html',
+  './code_example_templates/*.erb'].concat(SASS_SOURCES);
+const GENERATED_CSS = DEST + '/*.css';
 
 gulp.task('sass', () => {
-  gulp.src(SASS_SOURCES)
+  return gulp.src(SASS_SOURCES)
     .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -27,18 +29,12 @@ gulp.task('sass', () => {
 });
 
 gulp.task('hologram', () => {
-  gulp.src(STYLE_GUIDE_SOURCES)
+  return gulp.src(STYLE_GUIDE_SOURCES)
     .pipe(hologram());
 });
 
-gulp.task('watch', () => {
-  gulp.watch([STYLE_GUIDE_SOURCES, SASS_SOURCES], ['sass']);
-  gulp.watch(STYLE_GUIDE_SOURCES, ['hologram']);
-  gulp.watch(DEST, ['minify-css']);
-});
-
 gulp.task('minify-css', () => {
-  return gulp.src(DEST + '/*.css')
+  return gulp.src(GENERATED_CSS)
     .pipe(sourcemaps.init())
     .pipe(cleanCSS({debug: true}, details => {
       console.log(`file: ${details.name}
@@ -49,3 +45,11 @@ gulp.task('minify-css', () => {
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(DIST));
 });
+
+gulp.task('watch', () => {
+  gulp.watch([STYLE_GUIDE_SOURCES, SASS_SOURCES], ['sass']);
+  gulp.watch(STYLE_GUIDE_SOURCES, ['hologram']);
+  gulp.watch(GENERATED_CSS, ['minify-css']);
+});
+
+gulp.task('default', ['sass', 'hologram', 'watch']);
